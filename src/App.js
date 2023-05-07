346de1aa998e0533a93e8ef4537fac3621d6ac1f
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-05-07 23:14:03 +0200

    Add a box with 2 input fields

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
-import logo from './logo.svg';
+import { useState } from 'react';
 
 export default function App() {
+  const [topTextBox, setTopTextBox] = useState('');
+  const [bottomTextBox, setBottomTextBox] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          value={topTextBox}
+          onChange={(event) => setTopTextBox(event.currentTarget.value)}
+        />
+        <br />
+        <input
+          value={bottomTextBox}
+          onChange={(event) => setBottomTextBox(event.currentTarget.value)}
+        />
+      </div>
     </div>
   );
 }
