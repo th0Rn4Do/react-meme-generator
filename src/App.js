649aa837a2d7751a0c6613d0398644c645402bca
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-05-09 10:38:20 +0200

    Add header, main, footer

--- src/App.js
+++ src/App.js
@@ -10,42 +10,50 @@ export default function App() {
   const [bottomTextBox, setBottomTextBox] = useState('');
   const [memeTemplate, setMemeTemplate] = useState('');
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
-    >
-      <div className="App">
-        <div>
-          <label htmlFor="topTextBox">Top text: </label>
-          <input
-            value={topTextBox}
-            onChange={(event) => setTopTextBox(event.currentTarget.value)}
-            id="topTextBox"
-          />
-          <br />
-          <label htmlFor="bottomTextBox">Bottom text: </label>
-          <input
-            value={bottomTextBox}
-            onChange={(event) => setBottomTextBox(event.currentTarget.value)}
-            id="bottomTextBox"
-          />
-          <br />
-          <label htmlFor="memeTemplate">Meme template </label>
-          <input
-            value={memeTemplate}
-            onChange={(event) => setMemeTemplate(event.currentTarget.value)}
-            id="memeTemplate"
-          />
-        </div>
-        <Image />
-        <br />
-        <button onClick={() => {}}>Create meme</button>
-        <br />
-        <button onClick={() => {}}>Download</button>
-        <br />
-      </div>
-      {/* <GetApiCall /> */}
-    </form>
+    <>
+      <h1>header</h1>
+      <main>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+        >
+          <div className="App">
+            <div>
+              <label htmlFor="topTextBox">Top text: </label>
+              <input
+                value={topTextBox}
+                onChange={(event) => setTopTextBox(event.currentTarget.value)}
+                id="topTextBox"
+              />
+              <br />
+              <label htmlFor="bottomTextBox">Bottom text: </label>
+              <input
+                value={bottomTextBox}
+                onChange={(event) =>
+                  setBottomTextBox(event.currentTarget.value)
+                }
+                id="bottomTextBox"
+              />
+              <br />
+              <label htmlFor="memeTemplate">Meme template </label>
+              <input
+                value={memeTemplate}
+                onChange={(event) => setMemeTemplate(event.currentTarget.value)}
+                id="memeTemplate"
+              />
+            </div>
+            <Image />
+            <br />
+            <button onClick={() => {}}>Create meme</button>
+            <br />
+            <button onClick={() => {}}>Download</button>
+            <br />
+          </div>
+          {/* <GetApiCall /> */}
+        </form>
+      </main>
+      <footer>my footer</footer>
+    </>
   );
 }
